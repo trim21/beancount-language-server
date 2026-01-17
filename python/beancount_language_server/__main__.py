b0dfdaa9ec557bfb5e0793be324855a2f8e5fdbf
Trim21 <trim21.me@gmail.com>
2026-01-17 19:44:54 +0800

    fix argv[0] when called with `python -m`

--- python/beancount_language_server/__main__.py
+++ python/beancount_language_server/__main__.py
@@ -1,3 +1,12 @@
+import sys
+
 from .wrapper import main
 
-raise SystemExit(main())
+
+def _main() -> int:
+    args = list(sys.argv)
+    args[0] = "python -m beancount_language_server"
+    return main(args)
+
+
+raise SystemExit(_main())
